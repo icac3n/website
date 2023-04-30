f9328d020592c8274d19d08613844e787e535b69
areeburrub <areeburrub@gmail.com>
2023-04-30 18:57:31 +0530

    add: meta tags to all pages for seo

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,25 +1,54 @@
 import React from "react";
 import Button from "@/components/button";
+import Head from "next/head";
 
 const callForPapers = () => {
 
     return (
-        <div className={"min-h-screen flex items-center justify-center"}>
-            <section className="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
-                <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-                    <div className="max-w-md text-center">
-                        <h2 className="mb-8 font-extrabold text-9xl text-red-800">
-                            <span className="sr-only">Error</span>404
-                        </h2>
-                        <p className="text-2xl font-semibold md:text-3xl">Sorry, we could not find this page.</p>
-                        <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things
-                            on our homepage.</p>
-                        <Button link={"/"}>Back to Homepage</Button>
+        <>
+            <Head>
+                <title>404 - ICAC3N - Galgotias College of Engineering</title>
+                <meta name="title" content="404 - ICAC3N- Galgotias College of Engineering"/>
+                <meta name="description"
+                      content="404 - 5th International Conference on Advances in Computing, Communication Control and Networking- ICAC3N"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
+
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://icac3n.in/"/>
+                <meta property="og:title" content="404 - ICAC3N- Galgotias College of Engineering"/>
+                <meta name="description"
+                      content="404 - 5th International Conference on Advances in Computing, Communication Control and Networking- ICAC3N"/>
+                <meta property="og:image"
+                      content="https://icac3n.in/api/og"/>
+
+
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content="https://icac3n.in/"/>
+                <meta property="404 - twitter:title" content="404 - ICAC3N- Galgotias College of Engineering"/>
+                <meta property="twitter:description"
+                      content="404 - 5th International Conference on Advances in Computing, Communication Control and Networking- ICAC3N"/>
+                <meta property="twitter:image"
+                      content="https://icac3n.in/api/og"/>
+            </Head>
+            <div className={"min-h-screen flex items-center justify-center"}>
+                <section className="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
+                    <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+                        <div className="max-w-md text-center">
+                            <h2 className="mb-8 font-extrabold text-9xl text-red-800">
+                                <span className="sr-only">Error</span>404
+                            </h2>
+                            <p className="text-2xl font-semibold md:text-3xl">Sorry, we could not find this page.</p>
+                            <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things
+                                on our homepage.</p>
+                            <Button link={"/"}>Back to Homepage</Button>
+                        </div>
                     </div>
-                </div>
-            </section>
-        </div>
-    )
+                </section>
+            </div>
+        </>
+    
+)
 
 }
 
--- src/pages/guidelines.tsx
+++ src/pages/guidelines.tsx
@@ -1,79 +1,109 @@
 import {StarterLayout} from "layout";
 import Button from "@/components/button";
+import Head from "next/head";
+import React from "react";
 
 const Guidelines = () => {
 
     return (
-        <div className={"prose mx-4 my-8"}>
-            <h1 className={"heading"}>Submission Guidelines</h1>
-            <p>Authors are solicited to contribute to the conference by submitting articles that illustrate research
-                results- projects, surveying works and industrial experiences that describe significant advances in
-                computing, communication and Automation. Topics of interest for submission include, but are not limited
-                to:</p>
-            <ul>
-                <li> Evolutionary Computing, Distributed and Parallel Processing, Cloud and Green Computing, IOT, Data
-                    Mining, Advance Database Management System
-                </li>
-                <li>Big Data, Data Analysis, Machine Learning and Artificial Intelligence, Mobile Computing, Artificial
-                    Neural Network, MANETs, Computer Networking
-                </li>
-                <li>Cyber security, Virtual Reality, Network Security, WSN, Multimedia Applications</li>
-                <li>Web Intelligence & Semantics</li>
+        <>
 
-                <li> Ubiquitous Computing & Networking</li>
+            <Head>
+                <title>Guidelines - ICAC3N- Galgotias College of Engineering</title>
+                <meta name="title" content="Guidelines - ICAC3N- Galgotias College of Engineering"/>
+                <meta name="description"
+                      content="Guidelines - 5th International Conference on Advances in Computing, Communication Control and Networking- ICAC3N"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
 
-                <li> Software Engineering & Information Systems</li>
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://icac3n.in/"/>
+                <meta property="og:title" content="Guidelines - ICAC3N- Galgotias College of Engineering"/>
+                <meta name="description"
+                      content="Guidelines - 5th International Conference on Advances in Computing, Communication Control and Networking- ICAC3N"/>
+                <meta property="og:image"
+                      content="https://icac3n.in/api/og"/>
 
-                <li> Intelligent Systems & Automation</li>
 
-                <li> Hardware design & Communication</li>
-
-                <li> Signal & Image Processing, VLSI Design</li>
-
-                <li> Wireless Sensor Networks, Security</li>
-
-                <li> Robotics & Automation</li>
-
-                <li> Communication Network and Systems</li>
-
-                <li> Smart Grid Technologies</li>
-
-                <li> Smart buildings Technologies</li>
-
-                <li>New Measurement technologies and Application, Fuzzy & Neural Networks</li>
-
-                <p>Interested authors are invited to submit full papers including results, figures and references.
-                    Papers will be accepted only by electronic submission through easy-chair link given below</p>
-
-                <Button link={"https://cmt3.research.microsoft.com/ICAC3N2023"} className={''} hideIcon={null}> Submit a Paper</Button>
-                <p>Authors need to submit the full final paper (Maximum 6 pages, double-column US letter size) as PDF
-                    using the IEEE templates. Extra pages beyond this would require additional page length charges. The
-                    additional payment required for extra pages is Rs.2000/page for Indian authors of all categories and
-                    USD 40/page for all categories of foreign authors. The IEEE paper template can be downloaded from
-                    the link given below.</p>
-
-                <Button className={''} hideIcon={null} link={"/documents/PAPER_ID_ICAC3N23.docx"}> Download IEEE Conference Template
-                </Button>
-                <h2 className={"heading"}>Plagiarism Policy</h2>
-                <p>ICAC3N-23 takes plagiarism very seriously and regard plagiarism as a professional misconduct. Papers
-                    will be screened for plagiarism and when identified the paper will be rejected.</p>
-
-                <h2 className={"heading"}>Important Message</h2>
-                <ol>
-                    <li>ICAC3N conference organizers have zero tolerance against plagiarism and paper formatting as per
-                        IEEE template. If author found violating registration guidelines and instructions provided from
-                        time to time at any stage during publication the registration of paper will be cancelled.
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content="https://icac3n.in/"/>
+                <meta property="Guidelines - twitter:title" content="Guidelines - ICAC3N- Galgotias College of Engineering"/>
+                <meta property="twitter:description"
+                      content="Guidelines - 5th International Conference on Advances in Computing, Communication Control and Networking- ICAC3N"/>
+                <meta property="twitter:image"
+                      content="https://icac3n.in/api/og"/>
+            </Head>
+            <div className={"prose mx-4 my-8"}>
+                <h1 className={"heading"}>Submission Guidelines</h1>
+                <p>Authors are solicited to contribute to the conference by submitting articles that illustrate research
+                    results- projects, surveying works and industrial experiences that describe significant advances in
+                    computing, communication and Automation. Topics of interest for submission include, but are not limited
+                    to:</p>
+                <ul>
+                    <li> Evolutionary Computing, Distributed and Parallel Processing, Cloud and Green Computing, IOT, Data
+                        Mining, Advance Database Management System
                     </li>
-                    <li>Transfer of e-copyright and presenting paper in conference is mandatory, without this paper will
-                        not be published on IEEE explore.
+                    <li>Big Data, Data Analysis, Machine Learning and Artificial Intelligence, Mobile Computing, Artificial
+                        Neural Network, MANETs, Computer Networking
                     </li>
-                    <li>Corresponding author (as per CMT) keep checking their e-mail regularly (including spam) in this
-                        regard and for supplying any other desired information on time.
-                    </li>
-                    <li>Registration fee once paid is non refundable.</li>
-                </ol>
-            </ul>
-        </div>
+                    <li>Cyber security, Virtual Reality, Network Security, WSN, Multimedia Applications</li>
+                    <li>Web Intelligence & Semantics</li>
+
+                    <li> Ubiquitous Computing & Networking</li>
+
+                    <li> Software Engineering & Information Systems</li>
+
+                    <li> Intelligent Systems & Automation</li>
+
+                    <li> Hardware design & Communication</li>
+
+                    <li> Signal & Image Processing, VLSI Design</li>
+
+                    <li> Wireless Sensor Networks, Security</li>
+
+                    <li> Robotics & Automation</li>
+
+                    <li> Communication Network and Systems</li>
+
+                    <li> Smart Grid Technologies</li>
+
+                    <li> Smart buildings Technologies</li>
+
+                    <li>New Measurement technologies and Application, Fuzzy & Neural Networks</li>
+
+                    <p>Interested authors are invited to submit full papers including results, figures and references.
+                        Papers will be accepted only by electronic submission through easy-chair link given below</p>
+
+                    <Button link={"https://cmt3.research.microsoft.com/ICAC3N2023"} className={''} hideIcon={null}> Submit a Paper</Button>
+                    <p>Authors need to submit the full final paper (Maximum 6 pages, double-column US letter size) as PDF
+                        using the IEEE templates. Extra pages beyond this would require additional page length charges. The
+                        additional payment required for extra pages is Rs.2000/page for Indian authors of all categories and
+                        USD 40/page for all categories of foreign authors. The IEEE paper template can be downloaded from
+                        the link given below.</p>
+
+                    <Button className={''} hideIcon={null} link={"/documents/PAPER_ID_ICAC3N23.docx"}> Download IEEE Conference Template
+                    </Button>
+                    <h2 className={"heading"}>Plagiarism Policy</h2>
+                    <p>ICAC3N-23 takes plagiarism very seriously and regard plagiarism as a professional misconduct. Papers
+                        will be screened for plagiarism and when identified the paper will be rejected.</p>
+
+                    <h2 className={"heading"}>Important Message</h2>
+                    <ol>
+                        <li>ICAC3N conference organizers have zero tolerance against plagiarism and paper formatting as per
+                            IEEE template. If author found violating registration guidelines and instructions provided from
+                            time to time at any stage during publication the registration of paper will be cancelled.
+                        </li>
+                        <li>Transfer of e-copyright and presenting paper in conference is mandatory, without this paper will
+                            not be published on IEEE explore.
+                        </li>
+                        <li>Corresponding author (as per CMT) keep checking their e-mail regularly (including spam) in this
+                            regard and for supplying any other desired information on time.
+                        </li>
+                        <li>Registration fee once paid is non refundable.</li>
+                    </ol>
+                </ul>
+            </div>
+        </>
     )
 
 }
