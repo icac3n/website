8238ac43ab0d73711e29b7c7e48579c3a9d6bda0
International Conference on Advances in Computing, Communication Control and Networking <131188674+icac3n@users.noreply.github.com>
2024-07-30 14:45:54 +0530

    fix: navbar links

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -228,24 +228,27 @@ function Index() {
 
                         <ul className="flex flex-col gap-3 md:text-sm lg:gap-5 justify-between md:items-center items-start font-medium md:p-0 rounded-lg md:flex-row md:mt-0 md:border-0">
                             {links.map((link, index) => (
-                                
-                                link.enabled && link.link &&
-                                <div key={index} className={'md:w-auto w-full'}>
+                                <>
                                     {
-                                        <>
-                                            <li key={index}
-                                                className={`cursor-pointer text-white`}>
-                                                <Link href={link.link}>
-                                                    {link.title.toUpperCase()}
-                                                </Link>
-                                            </li>
-                                        </>
+                                        link.enabled && link.link &&
+                                        <div key={index} className={'md:w-auto w-full'}>
+                                            {
+                                                <>
+                                                    <li key={index}
+                                                        className={`cursor-pointer text-white`}>
+                                                        <Link href={link.link}>
+                                                            {link.title.toUpperCase()}
+                                                        </Link>
+                                                    </li>
+                                                </>
+                                            }
+                                            {
+                                                link.enabled && link.dropdown &&
+                                                <Dropdown title={link.title} dropdown={link.dropdown}/>
+                                            }
+                                        </div>
                                     }
-                                    {
-                                        link.enabled && link.dropdown &&
-                                        <Dropdown title={link.title} dropdown={link.dropdown}/>
-                                    }
-                                </div>
+                                </>
 
                             ))}
                         </ul>
