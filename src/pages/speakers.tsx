eb627341f9e77f6c3e39e88427cfe946941924e5
International Conference on Advances in Computing, Communication Control and Networking <131188674+icac3n@users.noreply.github.com>
2024-07-30 14:54:30 +0530

    fix: speakers page

--- src/pages/speakers.tsx
+++ src/pages/speakers.tsx
@@ -35,37 +35,39 @@ const Speakers = () => {
                 <h1 className={'text-3xl font-bold text-center mb-10'}>Past Speakers of ICACCCN 2023</h1>
                 <div className={'grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-5 gap-y-8 px-5'}>
                     {
-                        speakers.map((speaker, index) => {
-                            return (
-                                <div key={index}
-                                     className={'col-span-1 shadow-md grid grid-cols-3 gap-3 bg-gray-100 rounded-lg'}>
-                                    <div className={'col-span-1 w-full h-44'}>
-                                        <img src={speaker.image} className={'h-full w-full object-cover rounded-lg '} alt={speaker.name}/>
-                                    </div>
-                                    <div className={"flex flex-col gap-1 col-span-2 my-2"}>
-                                        <div className={"py-2"}>
-                                            {!speaker.national &&
-                                                <div
-                                                    className={"rounded-full w-fit font-semibold bg-blue-800 bg-opacity-20"}>
-                                                    <p className={"px-2 py-0.5 text-sm text-blue-800"}>International
-                                                        Speaker</p>
-                                                </div>
-                                            }
-                                            {
-                                                speaker.national &&
-                                                <div
-                                                    className={"rounded-full font-semibold w-fit bg-amber-600 bg-opacity-20"}>
-                                                    <p className={"px-2 py-0.5 text-sm text-amber-800"}>National Speaker</p>
-                                                </div>
-                                            }
+                        if speakers.length > 0 {
+                            speakers.map((speaker, index) => {
+                                return (
+                                    <div key={index}
+                                         className={'col-span-1 shadow-md grid grid-cols-3 gap-3 bg-gray-100 rounded-lg'}>
+                                        <div className={'col-span-1 w-full h-44'}>
+                                            <img src={speaker.image} className={'h-full w-full object-cover rounded-lg '} alt={speaker.name}/>
                                         </div>
-                                        <span className={'font-semibold text-lg'}>{speaker.name}</span>
-                                        <span
-                                            className={'font-light text-xs pr-2'}>{speaker.designation}</span>
+                                        <div className={"flex flex-col gap-1 col-span-2 my-2"}>
+                                            <div className={"py-2"}>
+                                                {!speaker.national &&
+                                                    <div
+                                                        className={"rounded-full w-fit font-semibold bg-blue-800 bg-opacity-20"}>
+                                                        <p className={"px-2 py-0.5 text-sm text-blue-800"}>International
+                                                            Speaker</p>
+                                                    </div>
+                                                }
+                                                {
+                                                    speaker.national &&
+                                                    <div
+                                                        className={"rounded-full font-semibold w-fit bg-amber-600 bg-opacity-20"}>
+                                                        <p className={"px-2 py-0.5 text-sm text-amber-800"}>National Speaker</p>
+                                                    </div>
+                                                }
+                                            </div>
+                                            <span className={'font-semibold text-lg'}>{speaker.name}</span>
+                                            <span
+                                                className={'font-light text-xs pr-2'}>{speaker.designation}</span>
+                                        </div>
                                     </div>
-                                </div>
-                            )
-                        })
+                                )
+                            })
+                        } 
                     }
                 </div>
                 {/*<h1 className={'text-lg text-center w-full'}>*/}
