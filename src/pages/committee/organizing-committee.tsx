5499a8de7331be64597b9455fb57c679630738e0
Areeb ur Rub <24785527+areeburrub@users.noreply.github.com>
2024-07-31 19:33:31 +0530

    Update organizing-committee.tsx

--- src/pages/committee/organizing-committee.tsx
+++ src/pages/committee/organizing-committee.tsx
@@ -2,185 +2,228 @@ import Head from "next/head";
 import React from "react";
 
 const OrganizingCommittee = () => {
-
-    const organizingCommittee = [
-
-        {
-            "Category": "Chief Patron",
-            "Members": [
-                {
-                    "Name": "Shri Suneel Galgotia",
-                    "Designation": "Chairman"
-                },
-                {
-                    "Name": "Dr. Dhruv Galgotia",
-                    "Designation": "CEO"
-                }
-            ]
-        },
-        {
-            "Category": "Patrons",
-            "Members": [
-                {
-                    "Name": "Prof. (Dr.) S.N. Singh",
-                    "Designation": "Director, ABV-IIITM, Gwalior"
-                },
-                {
-                    "Name": "Prof. (Dr.) S. K. Singh",
-                    "Designation": "IIIT, Allahabad"
-                },
-                {
-                    "Name": "Prof. (Dr.) Rajat Kumar Singh",
-                    "Designation": "IIIT, Allahabad"
-                },
-                {
-                    "Name": "Prof. (Dr.) Avadhesh Kumar",
-                    "Designation": "Pro-Vice-Chancellor, GU"
-                },
-                {
-                    "Name": "Gp. Capt. (R) Dr. P K Chopra, VSM",
-                    "Designation": "Director General, GEI"
-                }
-            ]
-        },
-        {
-            "Category": "Conference General Chair and Conference Convener",
-            "Members": [
-                {
-                    "Name": "Prof. (Dr.) Sachin Kumar",
-                    "Designation": "Professor and Head, CSE, GCET"
-                }
-            ]
-        },
-        {
-            "Category": "Conference Organizing Chair",
-            "Members": [
-                {
-                    "Name": "Prof. (Dr.) Vinit Kumar",
-                    "Designation": "Professor and Head, MCA, GCET"
-                }
-            ]
-        },
-        {
-            "Category": "Technical Program Chair",
-            "Members": [
-                {
-                    "Name": "Prof (Dr.) Krishan Kumar Saraswat",
-                    "Designation": "CSE, GCET"
-                }
-            ]
-        },
-        {
-            "Category": "Technical Program committee",
-            "Members": [
-                {
-                    "Name": "Prof (Dr.) Krishan Kumar Saraswat",
-                    "Designation": "CSE, GCET"
-                },
-                {
-                    "Name": "Prof (Dr.) Jaya Sinha",
-                    "Designation": "Professor, CSE, GCET"
-                }
-            ]
-        },
-        {
-            "Category": "Publication Chair and Publicity Chair",
-            "Members": [
-                {
-                    "Name": "Prof. (Dr.) Sanjeev Kumar Singh",
-                    "Designation": "Professor & Head, IT, GCET"
-                }
-            ]
-        },
-        {
-            "Category": "Publication Committee",
-            "Members": [
-                {
-                    "Name": "Prof. (Dr.) Sanjeev Kumar Singh",
-                    "Designation": "Professor & Head, IT, GCET"
-                },
-                {
-                    "Name": "Prof. (Dr.) Veena Mittal",
-                    "Designation": "IT, GCET"
-                }
-            ]
-        },
-        {
-            "Category": "Publicity Committee",
-            "Members": [
-                {
-                    "Name": "Prof. (Dr.) Sanjeev Kumar Singh",
-                    "Designation": "Professor & Head, IT, GCET"
-                },
-                {
-                    "Name": "Prof. (Dr.) Suman Avdhesh Yadav",
-                    "Designation": "Professor, CSE, Amity University, Gr.Noida"
-                },
-                {
-                    "Name": "Prof. (Dr.) Jaya Sinha",
-                    "Designation": "Professor, CSE, GCET"
-                },
-                {
-                    "Name": "Prof. Nripendra Kumar Singh",
-                    "Designation": "MCA, GCET"
-                }
-            ]
-        },
-        {
-            "Category": "Treasurer/Finance committee",
-            "Members": [
-                {
-                    "Name": "Prof (Dr.) Jaya Sinha",
-                    "Designation": "Professor, CSE, GCET"
-                },
-                {
-                    "Name": "Prof. (Dr.) Nripendra Kumar Singh",
-                    "Designation": "MCA, GCET"
-                }
-            ]
-        },
-        {
-            "Category": "Track Chairs",
-            "Members": [
-                {
-                    "Name": "Prof. (Dr.) Chien-Ming Chen",
-                    "Designation": "Professor, School of Artificial Intelligence (School of Future Technology), Nanjing University of Information Science and Technology, China"
-                },
-                {
-                    "Name": "Prof. (Dr.) R. Swaminathan",
-                    "Designation": "Professor and Head, ECE, GCET"
-                },
-                {
-                    "Name": "Prof. (Dr.) Saru Kumari",
-                    "Designation": "Associate Professor, CCS University, Meerut, Uttar Pradesh"
-                }
-            ]
-        },
-        {
-            "Category": "Hospitality Committee",
-            "Members": [
-                {
-                    "Name": "Prof. (Dr.) Pallavi Goel",
-                    "Designation": "CSE, GCET"
-                },
-                {
-                    "Name": "Prof. (Dr.) Inderjeet Kaur",
-                    "Designation": "CSE, GCET"
-                }
-            ]
-        },
-        {
-            "Category": "Website Committee",
-            "Members": [
-                {
-                    "Name": "Prof (Dr.) Krishan Kumar Saraswat",
-                    "Designation": "CSE, GCET"
-                }
-            ]
-        }
-
-
-    ]
+    
+const organizingCommittee = [
+    {
+        "Category": "Chief Patrons",
+        "Members": [
+            {
+                "Name": "Shri Suneel Galgotia",
+                "Designation": "Chairman, Galgotias Educational Institutions"
+            },
+            {
+                "Name": "Dr. Dhruv Galgotia",
+                "Designation": "CEO, Galgotias Educational Institutions"
+            }
+        ]
+    },
+    {
+        "Category": "Patrons",
+        "Members": [
+            {
+                "Name": "Prof.(Dr.) K. Mallikharjuna Babu",
+                "Designation": "Vice Chancellor, Galgotias University, Greater Noida"
+            },
+            {
+                "Name": "Prof.(Dr.) Avadhesh Kumar",
+                "Designation": "Pro-Vice Chancellor, Galgotias University, Greater Noida"
+            },
+            {
+                "Name": "Prof.(Dr.) Vikram Bali",
+                "Designation": "Director, Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Co-Patrons",
+        "Members": [
+            {
+                "Name": "Prof. Brijesh Kumar",
+                "Designation": "Professor & Director (Planning), Dept. of IT, Indira Gandhi Delhi Technical University for Women, Delhi"
+            }
+        ]
+    },
+    {
+        "Category": "General Chairs",
+        "Members": [
+            {
+                "Name": "Prof.(Dr.) Vikram Bali",
+                "Designation": "Director, Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Prof. Brijesh Singh",
+                "Designation": "Dean Academics, Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Conference Convener",
+        "Members": [
+            {
+                "Name": "Prof.(Dr.) Pushpa Choudhary",
+                "Designation": "HOD CSE & Allied Branches"
+            }
+        ]
+    },
+    {
+        "Category": "Conference Organizing Chairs",
+        "Members": [
+            {
+                "Name": "Prof.(Dr.) Pushpa Choudhary",
+                "Designation": "HOD CSE & Allied Specialized Branches, Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Prof.(Dr.) Yashveer Singh",
+                "Designation": "Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Conference Co-Conveners",
+        "Members": [
+            {
+                "Name": "Dr. Krishan Kumar Saraswat",
+                "Designation": "Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Dr. Vandna Rani Verma",
+                "Designation": "Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Technical Program Chair",
+        "Members": [
+            {
+                "Name": "Prof.(Dr.) Arvinda Kushwaha",
+                "Designation": "Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Publication Chair",
+        "Members": [
+            {
+                "Name": "Prof.(Dr.) Ramveer Singh",
+                "Designation": "Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Publication Committee",
+        "Members": [
+            {
+                "Name": "Dr. Tanu Shree",
+                "Designation": "Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Dr. Chitrangada Chaubey",
+                "Designation": "Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Publicity Chair",
+        "Members": [
+            {
+                "Name": "Prof.(Dr.) Pallavi Goel",
+                "Designation": "Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Treasurer/Finance Committee",
+        "Members": [
+            {
+                "Name": "Dr. Sanjay Kumar",
+                "Designation": "Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Anuj Kumar Dwivedi",
+                "Designation": "Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Track Chairs",
+        "Members": [
+            {
+                "Name": "Dr. Arvinda Kushwaha",
+                "Designation": "Data Communication & Networks, Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Prof.(Dr.) Sanjay Kumar",
+                "Designation": "Artificial Intelligence & Expert Systems, Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Prof.(Dr.) Sachin Kumar",
+                "Designation": "Sustainable Technology, Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Prof.(Dr.) Ramveer Singh",
+                "Designation": "Data & System Security, Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Prof.(Dr.) Sachi Gupta",
+                "Designation": "Computer Vision & Learning, Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Dr. Sunil Kumar",
+                "Designation": "Data Analytics & Internet of Things, Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Website Committee",
+        "Members": [
+            {
+                "Name": "Devraj",
+                "Designation": "Assistant Professor, CSE, Galgotias College of Engineering and Technology, Greater Noida"
+            },
+            {
+                "Name": "Amit Pratap Singh",
+                "Designation": "Assistant Professor, CSE, Galgotias College of Engineering and Technology, Greater Noida"
+            }
+        ]
+    },
+    {
+        "Category": "Technical Program Committee & Advisory Committee",
+        "Members": [
+            {
+                "Name": "Prof. Ravi Shankar Singh",
+                "Designation": "Department of Computer Science and Engineering, Indian Institute of Technology, IIT BHU, Varanasi"
+            },
+            {
+                "Name": "Prof. Vivek Kumar Singh",
+                "Designation": "University of Delhi"
+            },
+            {
+                "Name": "Prof. Divakar Yadav",
+                "Designation": "IGNOU, Delhi"
+            },
+            {
+                "Name": "Prof. Deo Prakash Vidyarthi",
+                "Designation": "JNU, Delhi"
+            },
+            {
+                "Name": "Dr. Anand Sharma",
+                "Designation": "MNNIT, Allahabad"
+            },
+            {
+                "Name": "Dr. Rajiv Kumar Singh",
+                "Designation": "IET, Lucknow"
+            },
+            {
+                "Name": "Dr. Anshul Verma",
+                "Designation": "BHU, Varanasi"
+            },
+            {
+                "Name": "Dr. Avinash Kumar Pandey",
+                "Designation": "IIIT, Lucknow"
+            }
+        ]
+    }
+];
 
     return (
         <>
@@ -211,46 +254,44 @@ const OrganizingCommittee = () => {
             </Head>
             <div className={"prose mx-4"} itemScope itemType="https://schema.org/Organization">
                 <h1 className={"heading"} itemProp="name">ORGANISING COMMITTEE</h1>
-
-                <h3>Yet to be announced</h3>
                 {
-                    // organizingCommittee.map((committee, index) => {
-                    //     return (
-                    //         <div key={index} itemProp="department">
-                    //             <h2 className={"heading"} itemProp="name" id={committee.Category}>{committee.Category}</h2>
-                    //             <ul>
-                    //                 {
-                    //                     committee.Members.map((member, index) => {
-                    //                         return (
-                    //                             <li key={index} itemProp="member" id={member.Name}>
-                    //                                 <span className="font-semibold" itemProp="name">{member.Name}</span><br /><span
-                    //                                     itemProp="jobTitle" className="text-sm">{member.Designation}</span>
-                    //                             </li>
-                    //                         )
-                    //                     })
-                    //                 }
-                    //                 {/* {
-                    //                     committee.Students != undefined &&
-                    //                     <li className="list-none">
-                    //                         <h3 className={"heading"} itemProp="name">Student Co-Ordinator{committee?.Students?.length == 1 ? '' : 's'}</h3>
-                    //                         <ul>
-                    //                             {
-                    //                                 committee?.Students.map((member, index) => {
-                    //                                     return (
-                    //                                         <li key={index} itemProp="member" id={member.Name}>
-                    //                                             <span itemProp="name">{member.Name}</span>, <span
-                    //                                                 itemProp="jobTitle">{member.Designation}</span>
-                    //                                         </li>
-                    //                                     )
-                    //                                 })
-                    //                             }
-                    //                         </ul>
-                    //                     </li>
-                    //                 } */}
-                    //             </ul>
-                    //         </div>
-                    //     )
-                    // })
+                    organizingCommittee.map((committee, index) => {
+                         return (
+                             <div key={index} itemProp="department">
+                                 <h2 className={"heading"} itemProp="name" id={committee.Category}>{committee.Category}</h2>
+                                 <ul>
+                                     {
+                                         committee.Members.map((member, index) => {
+                                             return (
+                                                 <li key={index} itemProp="member" id={member.Name}>
+                                                     <span className="font-semibold" itemProp="name">{member.Name}</span><br /><span
+                                                         itemProp="jobTitle" className="text-sm">{member.Designation}</span>
+                                                 </li>
+                                             )
+                                         })
+                                     }
+                                     {/* {
+                                         committee.Students != undefined &&
+                                         <li className="list-none">
+                                             <h3 className={"heading"} itemProp="name">Student Co-Ordinator{committee?.Students?.length == 1 ? '' : 's'}</h3>
+                                             <ul>
+                                                 {
+                                                     committee?.Students.map((member, index) => {
+                                                         return (
+                                                             <li key={index} itemProp="member" id={member.Name}>
+                                                                 <span itemProp="name">{member.Name}</span>, <span
+                                                                     itemProp="jobTitle">{member.Designation}</span>
+                                                             </li>
+                                                         )
+                                                     })
+                                                 }
+                                             </ul>
+                                         </li>
+                                     } */}
+                                 </ul>
+                             </div>
+                         )
+                     })
                 }
 
             </div>
