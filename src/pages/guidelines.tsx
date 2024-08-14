f2acb4970070e61b3c9ea9e881165e35398d54e0
Areeb ur Rub <24785527+areeburrub@users.noreply.github.com>
2024-08-15 02:56:13 +0530

    fix: guidelines page

--- src/pages/guidelines.tsx
+++ src/pages/guidelines.tsx
@@ -73,9 +73,9 @@ const Guidelines = () => {
                     <li>New Measurement technologies and Application, Fuzzy & Neural Networks</li>
 
                     <p>Interested authors are invited to submit full papers including results, figures and references.
-                        Papers will be accepted only by electronic submission through easy-chair link given below</p> */}
+                        Papers will be accepted only by electronic submission through easy-chair link given below</p> 
 
-                    {/* <Button link={"https://cmt3.research.microsoft.com/ICRTICC2023"} className={''} hideIcon={null}> Submit a Paper</Button>
+                    <Button link={"https://cmt3.research.microsoft.com/ICRTICC2023"} className={''} hideIcon={null}> Submit a Paper</Button>
                     <p>Authors need to submit the full final paper (Maximum 6 pages, double-column US letter size) as PDF
                         using the IEEE templates. Extra pages beyond this would require additional page length charges. The
                         additional payment required for extra pages is Rs.2000/page for Indian authors of all categories and
@@ -83,7 +83,7 @@ const Guidelines = () => {
                         the link given below.</p>
 
                     <Button className={''} hideIcon={null} link={"/documents/PAPER_ID_ICRTICC23.docx"}> Download IEEE Conference Template
-                    </Button> */}
+                    </Button> 
                     <h2 className={"heading"}>Plagiarism Policy</h2>
                     <p>ICRTICC-23 takes plagiarism very seriously and regard plagiarism as a professional misconduct. Papers
                         will be screened for plagiarism and when identified the paper will be rejected.</p>
@@ -101,7 +101,7 @@ const Guidelines = () => {
                         </li>
                         <li>Registration fee once paid is non refundable.</li>
                     </ol>
-                </ul>
+                </ul> */}
             </div>
         </>
     )
